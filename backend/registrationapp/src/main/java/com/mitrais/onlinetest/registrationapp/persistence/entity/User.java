cb31ff5bc5779b8d849a65757dea0690f37b26df
Akbar Binaji <akbar.binaji@gmail.com>
2019-12-11 22:39:15 +0700

    add User entity

---
+++ backend/registrationapp/src/main/java/com/mitrais/onlinetest/registrationapp/persistence/entity/User.java
@@ -0,0 +1,64 @@
+package com.mitrais.onlinetest.registrationapp.persistence.entity;
+/*
+ * Dear Maintainer,
+ *
+ * When I wrote this code, only I and God knew what it was. Now, only God knows!
+ *
+ * So, If you're done, trying to 'optimize' this routine (and failed).
+ * Please, increment the following counter as a warning to the next guy:
+ * total_hours_wasted_here: 999;
+ *
+ * Sincerely Yours, Hooman
+ */
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String mobileNumber;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Column(length = 1)
+    private String gender;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+}
