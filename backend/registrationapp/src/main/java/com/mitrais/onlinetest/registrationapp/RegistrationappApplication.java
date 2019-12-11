de309e841e9fc66559b802aba9db17e6cb2832b2
Akbar Binaji <akbar.binaji@gmail.com>
2019-12-11 22:40:26 +0700

    update to enable jpa auditing

--- backend/registrationapp/src/main/java/com/mitrais/onlinetest/registrationapp/RegistrationappApplication.java
+++ backend/registrationapp/src/main/java/com/mitrais/onlinetest/registrationapp/RegistrationappApplication.java
@@ -2,8 +2,10 @@ package com.mitrais.onlinetest.registrationapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RegistrationappApplication {
 
 	public static void main(String[] args) {
