e353609defef95d1049e2a9f3604ebebe0da2761
Akbar Binaji <akbar.binaji@gmail.com>
2019-12-11 22:40:47 +0700

    add user repository

---
+++ backend/registrationapp/src/main/java/com/mitrais/onlinetest/registrationapp/persistence/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.mitrais.onlinetest.registrationapp.persistence.repository;
+/*
+ * Dear Maintainer,
+ *
+ * When I wrote this code, only I and God knew what it was. Now, only God knows!
+ *
+ * So, If you're done, trying to 'optimize' this routine (and failed).
+ * Please, increment the following counter as a warning to the next guy:
+ * total_hours_wasted_here: 999;
+ *
+ * Sincerely Yours, Hooman
+ */
+
+import com.mitrais.onlinetest.registrationapp.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
